510219226a7b32ff0fc47edf03c5f29ffcb72315
matmutant <matmutant@users.noreply.github.com>
2016-07-20 19:29:59 +0200

    Add files via upload

---
+++ systemd/system/trim3.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+OutputFile="/home/matmutant/trim-output"
+Touch="/bin/touch"
+Date="/bin/date"
+Fstrim="/sbin/fstrim"
+if [ ! -w "$OutputFile" ]; then
+	#if file doesn't exist it is created
+	$Touch "$OutputFile"
+fi
+#appends date to file and then performs trim to / dir
+$Date >> "$OutputFile" && $Fstrim -v / >> "$OutputFile"
+
+exit 0
